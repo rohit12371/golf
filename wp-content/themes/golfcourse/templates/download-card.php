94914331ea239177d613de3b165605e416a3462a
rohit arora <7417rohitarora@gmail.com>
2019-02-02 23:55:20 +0530

    first commikt

--- wp-content/themes/golfcourse/templates/download-card.php
+++ wp-content/themes/golfcourse/templates/download-card.php
@@ -5,7 +5,8 @@ $currentUserId=get_current_user_id();
 $userData=get_userdata($currentUserId);
 $dompdf = new DOMPDF();
 $link=ABSPATH."theme/golfcourse/assets/css/bootstrap.min.css";
-$imglink=ABSPATH."theme/golfcourse/assets\img\S.E.A.platinCARD_2019_druck.webp";
+$imglink=ABSPATH."wp-content/themes/golfcourse/assets\img\S.E.A.platinCARD_2019_druck.webp";
+
 $html.='<link href="'.$link.'" rel="stylesheet" type="text/css" />';
 $html.='<div class="container">';
 $html.='<div class="row spacer">';
@@ -13,6 +14,7 @@ $html.='<div class="container" style="margin:50px 0px 40px;">';
 $html.='<div class="row">';
 $html.='<div class="col-md-12 userdata">';
 $html.='<div style="background:url("'.$imglink.'") no-repeat center; height:400px;">';
+$html.="<span style='position: relative; top:271px; left:352px; font-size:12px;     font-family: 'Fjalla One', sans-serif!important;'>".$imglink."</span></div><div class='text-center'></div></div></div></div></div></div>";
 $html.="<span style='position: relative; top:271px; left:352px; font-size:62px;     font-family: 'Fjalla One', sans-serif!important;'>".$userData->user_login."</span></div><div class='text-center'></div></div></div></div></div></div>";
 $dompdf->load_html($html);
 $dompdf->set_paper('A4');
